949f370f757be8ed7ac96073676443ca13d798d1
Jeremy Xue <gg831006@gmail.com>
2019-04-12 10:50:15 +0800

    docs(ViewController): Set up buttons & configuration buttons

--- GesturePassword/ViewController.swift
+++ GesturePassword/ViewController.swift
@@ -10,10 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttons: [UIButton]! {
+        didSet {
+            configurationButtons()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    private func configurationButtons() {
+        buttons.forEach { (button) in
+            button.layer.cornerRadius = button.bounds.height / 2
+            button.layer.borderWidth = 3
+            button.layer.borderColor = UIColor.black.cgColor
+            button.clipsToBounds = true
+            button.setTitle("●", for: .normal)
+            button.tintColor = UIColor.black
+            button.addTarget(self, action: #selector(touchDragEnter(_:)), for: .touchDragEnter)
+            button.addTarget(self, action: #selector(touchDragExit(_:)), for: .touchDragExit)
+        }
+    }
+    
+    @objc func touchDragEnter(_ sender: UIButton) {
+        print("touchDragEnter")
+    }
+    
+    @objc func touchDragExit(_ sender: UIButton) {
+        print("touchDragExit")
+    }
+    
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        print(touches.first?.location(in: view))
+//    }
 
 
 }
