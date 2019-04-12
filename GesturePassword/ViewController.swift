969b5fb1f6500421a38e388d257b18e95a9cf305
Jeremy Xue <gg831006@gmail.com>
2019-04-12 12:39:58 +0800

    docs(ViewController): Add drawLine func & properties

--- GesturePassword/ViewController.swift
+++ GesturePassword/ViewController.swift
@@ -9,44 +9,106 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet var buttons: [UIButton]! {
-        didSet {
-            configurationButtons()
-        }
-    }
     
+    private var currentPoint: CGPoint?
+    private var selectedPassword = [Int]()
+    private var lineLayers = [CAShapeLayer]()
+    private var row = 3
+    private let buttonTag = -1
+    
+    @IBOutlet weak var gestureCollectionView: UICollectionView!
+    
+    @IBAction func reset(_ sender: UIButton) {
+        lineLayers.forEach { (layer) in
+            layer.removeFromSuperlayer()
+        }
+        selectedPassword.removeAll()
+        gestureCollectionView.reloadData()
+        currentPoint = nil
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        gestureCollectionView.dataSource = self
+        gestureCollectionView.delegate = self
     }
     
-    private func configurationButtons() {
-        buttons.forEach { (button) in
-            button.layer.cornerRadius = button.bounds.height / 2
-            button.layer.borderWidth = 3
+    private func configurationButton(_ cell: UICollectionViewCell, indexPath: IndexPath) {
+        guard let button = cell.viewWithTag(buttonTag) as? UIButton else { return }
+        cell.layoutIfNeeded()
+        button.layer.cornerRadius = button.bounds.height / 2
+        button.layer.borderWidth = 3
+        if !selectedPassword.contains(indexPath.row) {
             button.layer.borderColor = UIColor.black.cgColor
-            button.clipsToBounds = true
-            button.setTitle("●", for: .normal)
             button.tintColor = UIColor.black
-            button.addTarget(self, action: #selector(touchDragEnter(_:)), for: .touchDragEnter)
-            button.addTarget(self, action: #selector(touchDragExit(_:)), for: .touchDragExit)
+        } else {
+            button.layer.borderColor = UIColor.red.cgColor
+            button.tintColor = UIColor.red
         }
+        button.clipsToBounds = true
+        button.isUserInteractionEnabled = false
     }
     
-    @objc func touchDragEnter(_ sender: UIButton) {
-        print("touchDragEnter")
+    private func drawLine(to point: CGPoint) {
+        if let currentPoint = currentPoint {
+            let shapeLayer = CAShapeLayer()
+            shapeLayer.frame = gestureCollectionView.bounds
+            shapeLayer.fillColor = nil
+            shapeLayer.lineWidth = 3
+            shapeLayer.strokeColor = UIColor.red.cgColor
+            let path = UIBezierPath()
+            path.move(to: currentPoint)
+            path.addLine(to: point)
+            shapeLayer.path = path.cgPath
+            shapeLayer.position = gestureCollectionView.center
+            shapeLayer.lineCap = .round
+            view.layer.addSublayer(shapeLayer)
+            lineLayers.append(shapeLayer)
+        }
+        currentPoint = point
     }
-    
-    @objc func touchDragExit(_ sender: UIButton) {
-        print("touchDragExit")
-    }
-    
-//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        print(touches.first?.location(in: view))
-//    }
 
+}
 
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return row * row
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        configurationButton(cell, indexPath: indexPath)
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if selectedPassword.contains(indexPath.row) {
+            return
+        }
+        let cell = collectionView.cellForItem(at: indexPath)
+        drawLine(to: cell!.center)
+        selectedPassword.append(indexPath.row)
+        collectionView.reloadItems(at: [indexPath])
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width / CGFloat(row)
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
 
